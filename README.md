37fb619bbcd7fb587892b57a93381905357d7989
董兴龙 <ragnar@dongxinglongdeMacBook-Pro.local>
2019-08-27 22:34:32 +0800

    已完成

---
+++ README.md
@@ -0,0 +1,1 @@
+# Wechat-V2EX
---
+++ pages/node/node.js
@@ -0,0 +1,76 @@
+// pages/node/node.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    nodes:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getData();
+  },
+  getData: function() {
+    wx.request({
+      url: 'https://www.v2ex.com/api/nodes/all.json',
+      success:(res)=>{
+        this.setData({
+          nodes:res.data
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
